257a6873b8bf16b29fc8a4c1da25c1b95676a542
baekCode <sosaer88@gmail.com>
2020-09-07 10:00:49 +0900

    feat : play2048 구조 카피

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import AboveGame from './components/AboveGame';
+import Game from './components/Game';
 
 function App() {
-  return <div>Play Game</div>;
+  return (
+    <div className="container">
+      <Header />
+      <AboveGame />
+      <Game />
+    </div>
+  );
 }
 
 export default App;
