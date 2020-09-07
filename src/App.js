19e426e30d9373c43a8c6a833b22ed6c050c2e65
baekCode <sosaer88@gmail.com>
2020-09-07 13:47:32 +0900

    feat : Score 상태값 관리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import AboveGame from './components/AboveGame';
 import Game from './components/Game';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+  const [scroe, setScore] = useState(0);
+  const [bestScore, setBestScroe] = useLocalStorage('bestScore', 0);
+
+  useEffect(() => {
+    if (scroe > bestScore) {
+      setBestScroe(scroe);
+    }
+  });
+
   return (
     <div className="container">
-      <Header />
+      <Header scroe={scroe} bestScore={bestScore} />
       <AboveGame />
-      <Game />
+      <Game setScore={setScore} />
     </div>
   );
 }
